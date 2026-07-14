1f60a93423ba6707378db92d9ee69bbec2fa4349
Aline Timm <alinetimm1510@gmail.com>
2026-07-14 13:12:45 -0300

    cria pacote robot_navigation

---
+++ ros_ws/src/robot_navigation/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/mission.launch.py']),
+        ('share/' + package_name + '/config', ['config/mission.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Aline Timm',
+    maintainer_email='alinetimm1510@gmail.com',
+    description='Navegacao autonoma do TurtleBot3 Burger (Bug2 + wall-following) no ambiente stage5',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'navigator = robot_navigation.navigator:main',
+        ],
+    },
+)
